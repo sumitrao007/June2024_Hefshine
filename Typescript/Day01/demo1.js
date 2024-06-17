fd025dad7fc57757e0e64ac01003944946829377
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2024-06-17 09:51:18 +0530

    Day01

---
+++ Typescript/Day01/demo1.js
@@ -0,0 +1,27 @@
+// console.log("Hello World...")
+// console.log('Welcome To Angular 16 module')
+// console.log(`
+//             We Learn Typescript ...
+//     `);
+// var a1;
+// var a_1,a$1
+// data type 
+// number(int,long,float,double,short,unsigned),string(''/""),boolean(true/false)
+// any (number,string,boolean)
+// void => it is neutral data => it can not store any value 
+// misec null & undefined
+// it store value as well as data type as well => null is used for object , undefined is used for variable
+var a1 = 10; // forward declaration & defination 
+// console.log("Value of a is "+a1);
+// console.log('Value of a is '+a1);
+// console.log(`Value of a1 is ${a1} `)
+// var str!:string;
+// console.log("Value of str "+str);
+var a = 2.5;
+var c;
+c = 3.14;
+console.log("Value of c " + c);
+c = 'Sumit';
+console.log("Value of c " + c);
+c = true;
+console.log("Value of c " + c);
---
+++ Typescript/Day01/demo1.ts
@@ -0,0 +1,60 @@
+// console.log("Hello World...")
+// console.log('Welcome To Angular 16 module')
+// console.log(`
+//             We Learn Typescript ...
+//     `);
+
+// var a1;
+// var a_1,a$1
+
+// data type 
+// number(int,long,float,double,short,unsigned),string(''/""),boolean(true/false)
+// any (number,string,boolean)
+// void => it is neutral data => it can not store any value 
+// misec null & undefined
+// it store value as well as data type as well => null is used for object , undefined is used for variable
+
+var a1:number=10;  // forward declaration & defination 
+
+// console.log("Value of a is "+a1);
+// console.log('Value of a is '+a1);
+// console.log(`Value of a1 is ${a1} `)
+
+// var str!:string;
+// console.log("Value of str "+str);
+
+// var a=2.5;
+
+// var c:any;
+// c=3.14;
+// console.log("Value of c "+c)
+// c='Sumit';
+// console.log("Value of c "+c)
+// c=true;
+// console.log("Value of c "+c)
+
+// Type Assertion 
+// any,Object,unknown
+
+var a:any;
+
+// a.
+
+// 1 angle bracket =>used in ts file 
+// 2 as syntax => used in ts as well as html file 
+
+// 1 angle bracket =>used in ts file
+
+var temp=(<string> a);
+// temp.
+
+// 2 as syntax
+
+var temp1=(a as string );
+// temp1.
+
+
+
+
+
+
