fcc06fd84b17e0ec960bf637e720e6613c0c8794
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2024-06-21 09:08:14 +0530

    Day05

---
+++ Angular/Day05/employee/employee.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EmployeeComponent } from './employee.component';
+
+describe('EmployeeComponent', () => {
+  let component: EmployeeComponent;
+  let fixture: ComponentFixture<EmployeeComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [EmployeeComponent]
+    });
+    fixture = TestBed.createComponent(EmployeeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
